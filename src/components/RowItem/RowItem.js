f9c605bdf6233162f583573717e2a9823ef7e497
Daniel Loudon <daniel_loudon@choicehotels.com>
2017-03-07 14:17:11 -0700

    remove row ball item dependency

--- src/components/RowItem/RowItem.js
+++ src/components/RowItem/RowItem.js
@@ -5,20 +5,35 @@ import { incrementActiveRow, UPDATE_ACTIVE_ROW } from '../../redux/actions'
 import './RowItem.css'
 
 import Score from '../Score/Score'
-import RowItemBall from '../RowItemBalls/RowItemBall'
+// import RowItemBall from '../RowItemBalls/RowItemBall'
+import Ball from '../Ball/Ball'
 
 class RowItem extends Component {
   constructor(props) {
     super(props)
-    this.state = {guess: ['', '', '', ''], didGuess: false}
+    this.state = {
+      guesses: ['', '', '', ''],
+      didGuess: false
+    }
 
     this.evaluateRow = this.evaluateRow.bind(this)
+    this.changeBallColor = this.changeBallColor.bind(this)
+  }
+
+  checkAnswer(guess) {
+    console.log(this.answer);
+    console.log(guess);
   }
 
   evaluateRow(activeRowIndex) {
-    console.log('evaluating');
     this.setState({didGuess: true})
     this.props.dispatch(incrementActiveRow())
+  }
+
+  changeBallColor(index) {
+    let stateCopy = Object.assign({}, this.state)
+    stateCopy.guesses[index] = this.props.activeColor
+    this.setState(stateCopy)
   }
 
   render() {
@@ -27,10 +42,7 @@ class RowItem extends Component {
     return (
       <li className="row-item" key={index}>
         <span className="item-number">{index}</span>
-        <RowItemBall rowIndex={index} />
-        <RowItemBall rowIndex={index} />
-        <RowItemBall rowIndex={index} />
-        <RowItemBall rowIndex={index} />
+        {this.state.guesses.map((guess, i) => <Ball key={i} color={guess} onClick={() => this.changeBallColor(i, activeColor)} />)}
         {this.state.didGuess && <Score />}
         {!this.state.didGuess && index === activeRowIndex && <button onClick={this.evaluateRow}>Check</button>}
       </li>
@@ -46,7 +58,9 @@ RowItem.propTypes = {
 
 function mapStateToProps(state) {
   return {
-    activeRowIndex: state.index
+    activeColor: state.color,
+    activeRowIndex: state.index,
+    answer: state.answer
   }
 }
 
